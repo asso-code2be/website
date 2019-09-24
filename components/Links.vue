f63475f978addf153971062b3324bf9f8982c4d6
Christian Barras <tirabc@gmail.com>
2019-09-24 20:31:36 +0200

    improve: change links colors

--- components/Links.vue
+++ components/Links.vue
@@ -1,21 +1,21 @@
 <template>
-    <div class="py-4">
+    <div class="mt-4 py-4">
         <a  href="https://twitter.com/code2_be"
             target="_blank"
-            class="button--grey rounded-full"
+            class="py-3 px-4 rounded-lg bg-brand hover:bg-black text-black hover:text-brand"
         >Twitter</a>
         <a  href="https://www.linkedin.com/groups/8740339/"
             target="_blank"
-            class="button--grey"
+            class="py-3 px-4 rounded-lg bg-brand hover:bg-black text-black hover:text-brand"
         >LinkedIn</a>
         <a  href="https://github.com/asso-code2be/"
             target="_blank"
-            class="button--grey rounded-full"
+            class="py-3 px-4 rounded-lg bg-brand hover:bg-black text-black hover:text-brand"
         >Github</a>
         
         <a  href="mailto:contact@code2.be"
             target="_blank"
-            class="button--grey"
+            class="py-3 px-4 rounded-lg bg-brand hover:bg-black text-black hover:text-brand"
         >Email</a>
     </div>
 </template>
